bb453ef0069c625dd197c07b846597e1da854671
nguyenphuc355 <nguyenvanphuca9999@gmail.com>
2022-07-02 10:17:30 +0700

    update

--- vanphuc/src/ss1_java_introduction/exercise/ReadNumbersIntoWords.java
+++ vanphuc/src/ss1_java_introduction/exercise/ReadNumbersIntoWords.java
@@ -5,132 +5,308 @@ import java.util.Scanner;
 public class ReadNumbersIntoWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập số nguyên dương có tối đa 3 chữ số: ");
+        System.out.println("nhap so nguyen duong co toi da 3 chu so: ");
         int number = scanner.nextInt();
 
         if (number < 10) {
             switch (number) {
-                case 0 -> System.out.println("zero");
-                case 1 -> System.out.println("one");
-                case 2 -> System.out.println("two");
-                case 3 -> System.out.println("three");
-                case 4 -> System.out.println("four");
-                case 5 -> System.out.println("five");
-                case 6 -> System.out.println("six");
-                case 7 -> System.out.println("seven");
-                case 8 -> System.out.println("eight");
-                case 9 -> System.out.println("nine");
-                default -> System.out.println("out of ability!");
+                case 0:
+                    System.out.println("zero");
+                    break;
+                case 1 :
+                    System.out.println("one");
+                    break;
+                case 2:
+                    System.out.println("two");
+                    break;
+                case 3:
+                    System.out.println("three");
+                    break;
+                case 4:
+                    System.out.println("four");
+                    break;
+                case 5:
+                    System.out.println("five");
+                    break;
+                case 6:
+                    System.out.println("six");
+                    break;
+                case 7:
+                    System.out.println("seven");
+                    break;
+                case 8:
+                    System.out.println("eight");
+                    break;
+                case 9:
+                    System.out.println("nine");
+                    break;
+                default:
+                    System.out.println("out of ability!");
             }
         } else if (number < 20) {
             switch (number % 10) {
-                case 0 -> System.out.println("ten");
-                case 1 -> System.out.println("eleven");
-                case 2 -> System.out.println("twelve");
-                case 3 -> System.out.println("thirteen");
-                case 4 -> System.out.println("fourteen");
-                case 5 -> System.out.println("fifteen");
-                case 6 -> System.out.println("sixteen");
-                case 7 -> System.out.println("seventeen");
-                case 8 -> System.out.println("eighteen");
-                case 9 -> System.out.println("nineteen");
-                default -> System.out.println("out of ability!");
+                case 0:
+                    System.out.println("ten");
+                    break;
+                case 1:
+                    System.out.println("eleven");
+                    break;
+                case 2:
+                    System.out.println("twelve");
+                    break;
+                case 3:
+                    System.out.println("thirteen");
+                    break;
+                case 4:
+                    System.out.println("fourteen");
+                    break;
+                case 5:
+                    System.out.println("fifteen");
+                    break;
+                case 6:
+                    System.out.println("sixteen");
+                    break;
+                case 7:
+                    System.out.println("seventeen");
+                    break;
+                case 8:
+                    System.out.println("eighteen");
+                    break;
+                case 9:
+                    System.out.println("nineteen");
+                    break;
+                default:
+                    System.out.println("out of ability!");
             }
         } else if (number < 100) {
             switch (number / 10) {
-                case 2 -> System.out.print("twenty");
-                case 3 -> System.out.print("thirty");
-                case 4 -> System.out.print("forty");
-                case 5 -> System.out.print("fifty");
-                case 6 -> System.out.print("sixty");
-                case 7 -> System.out.print("seventy");
-                case 8 -> System.out.print("eighty");
-                case 9 -> System.out.print("ninety");
-                default -> System.out.println("out of ability!");
+                case 2:
+                    System.out.print("twenty");
+                    break;
+                case 3:
+                    System.out.print("thirty");
+                    break;
+                case 4:
+                    System.out.print("forty");
+                    break;
+                case 5:
+                    System.out.print("fifty");
+                    break;
+                case 6:
+                    System.out.print("sixty");
+                    break;
+                case 7:
+                    System.out.print("seventy");
+                    break;
+                case 8:
+                    System.out.print("eighty");
+                    break;
+                case 9:
+                    System.out.print("ninety");
+                    break;
+                default:
+                    System.out.println("out of ability!");
             }
 
             if (number % 10 != 0) {
                 switch (number % 10) {
-                    case 1 -> System.out.println("-one");
-                    case 2 -> System.out.println("-two");
-                    case 3 -> System.out.println("-three");
-                    case 4 -> System.out.println("-four");
-                    case 5 -> System.out.println("-five");
-                    case 6 -> System.out.println("-six");
-                    case 7 -> System.out.println("-seven");
-                    case 8 -> System.out.println("-eight");
-                    case 9 -> System.out.println("-nine");
-                    default -> System.out.println("out of ability!");
+                    case 1:
+                        System.out.println("-one");
+                        break;
+                    case 2:
+                        System.out.println("-two");
+                        break;
+                    case 3:
+                        System.out.println("-three");
+                        break;
+                    case 4:
+                        System.out.println("-four");
+                        break;
+                    case 5:
+                        System.out.println("-five");
+                        break;
+                    case 6:
+                        System.out.println("-six");
+                        break;
+                    case 7:
+                        System.out.println("-seven");
+                        break;
+                    case 8:
+                        System.out.println("-eight");
+                        break;
+                    case 9:
+                        System.out.println("-nine");
+                        break;
+                    default:
+                        System.out.println("out of ability!");
+                        break;
                 }
             }
         } else if (number < 1000) {
             switch (number / 100) {
-                case 1 -> System.out.print("one hundred");
-                case 2 -> System.out.print("two hundred");
-                case 3 -> System.out.print("three hundred");
-                case 4 -> System.out.print("four hundred");
-                case 5 -> System.out.print("five hundred");
-                case 6 -> System.out.print("six hundred");
-                case 7 -> System.out.print("seven hundred");
-                case 8 -> System.out.print("eight hundred");
-                case 9 -> System.out.print("nine hundred");
-                default -> System.out.println("out of ability!");
+                case 1:
+                    System.out.print("one hundred");
+                    break;
+                case 2:
+                    System.out.print("two hundred");
+                    break;
+                case 3:
+                    System.out.print("three hundred");
+                    break;
+                case 4:
+                    System.out.print("four hundred");
+                    break;
+                case 5:
+                    System.out.print("five hundred");
+                    break;
+                case 6:
+                    System.out.print("six hundred");
+                    break;
+                case 7:
+                    System.out.print("seven hundred");
+                    break;
+                case 8:
+                    System.out.print("eight hundred");
+                    break;
+                case 9:
+                    System.out.print("nine hundred");
+                    break;
+                default:
+                    System.out.println("out of ability!");
+                    break;
             }
 
-            int newNumber = number - (number/100)*100;
+            int newNumber = number - (number / 100) * 100;
             if (newNumber != 0) {
                 if (newNumber < 10) {
                     switch (newNumber) {
-                        case 1 -> System.out.println(" and one");
-                        case 2 -> System.out.println(" and two");
-                        case 3 -> System.out.println(" and three");
-                        case 4 -> System.out.println(" and four");
-                        case 5 -> System.out.println(" and five");
-                        case 6 -> System.out.println(" and six");
-                        case 7 -> System.out.println(" and seven");
-                        case 8 -> System.out.println(" and eight");
-                        case 9 -> System.out.println(" and nine");
-                        default -> System.out.println("out of ability!");
+                        case 1:
+                            System.out.println(" and one");
+                            break;
+                        case 2:
+                            System.out.println(" and two");
+                            break;
+                        case 3:
+                            System.out.println(" and three");
+                            break;
+                        case 4:
+                            System.out.println(" and four");
+                            break;
+                        case 5:
+                            System.out.println(" and five");
+                            break;
+                        case 6:
+                            System.out.println(" and six");
+                            break;
+                        case 7:
+                            System.out.println(" and seven");
+                            break;
+                        case 8:
+                            System.out.println(" and eight");
+                            break;
+                        case 9:
+                            System.out.println(" and nine");
+                            break;
+                        default:
+                            System.out.println("out of ability!");
                     }
                 } else if (newNumber < 20) {
                     switch (newNumber % 10) {
-                        case 0 -> System.out.println(" and ten");
-                        case 1 -> System.out.println(" and eleven");
-                        case 2 -> System.out.println(" and twelve");
-                        case 3 -> System.out.println(" and thirteen");
-                        case 4 -> System.out.println(" and fourteen");
-                        case 5 -> System.out.println(" and fifteen");
-                        case 6 -> System.out.println(" and sixteen");
-                        case 7 -> System.out.println(" and seventeen");
-                        case 8 -> System.out.println(" and eighteen");
-                        case 9 -> System.out.println(" and nineteen");
-                        default -> System.out.println("out of ability!");
+                        case 0:
+                            System.out.println(" and ten");
+                            break;
+                        case 1:
+                            System.out.println(" and eleven");
+                            break;
+                        case 2:
+                            System.out.println(" and twelve");
+                            break;
+                        case 3:
+                            System.out.println(" and thirteen");
+                            break;
+                        case 4:
+                            System.out.println(" and fourteen");
+                            break;
+                        case 5:
+                            System.out.println(" and fifteen");
+                            break;
+                        case 6:
+                            System.out.println(" and sixteen");
+                            break;
+                        case 7:
+                            System.out.println(" and seventeen");
+                            break;
+                        case 8:
+                            System.out.println(" and eighteen");
+                            break;
+                        case 9:
+                            System.out.println(" and nineteen");
+                            break;
+                        default:
+                            System.out.println("out of ability!");
                     }
                 } else if (newNumber < 100) {
                     switch (newNumber / 10) {
-                        case 2 -> System.out.print(" and twenty");
-                        case 3 -> System.out.print(" and thirty");
-                        case 4 -> System.out.print(" and forty");
-                        case 5 -> System.out.print(" and fifty");
-                        case 6 -> System.out.print(" and sixty");
-                        case 7 -> System.out.print(" and seventy");
-                        case 8 -> System.out.print(" and eighty");
-                        case 9 -> System.out.print(" and ninety");
-                        default -> System.out.println("out of ability!");
+                        case 2:
+                            System.out.print(" and twenty");
+                            break;
+                        case 3:
+                            System.out.print(" and thirty");
+                            break;
+                        case 4:
+                            System.out.print(" and forty");
+                            break;
+                        case 5:
+                            System.out.print(" and fifty");
+                            break;
+                        case 6:
+                            System.out.print(" and sixty");
+                            break;
+                        case 7:
+                            System.out.print(" and seventy");
+                            break;
+                        case 8:
+                            System.out.print(" and eighty");
+                            break;
+                        case 9:
+                            System.out.print(" and ninety");
+                            break;
+                        default:
+                            System.out.println("out of ability!");
+
                     }
 
                     if (newNumber % 10 != 0) {
                         switch (newNumber % 10) {
-                            case 1 -> System.out.println("-one");
-                            case 2 -> System.out.println("-two");
-                            case 3 -> System.out.println("-three");
-                            case 4 -> System.out.println("-four");
-                            case 5 -> System.out.println("-five");
-                            case 6 -> System.out.println("-six");
-                            case 7 -> System.out.println("-seven");
-                            case 8 -> System.out.println("-eight");
-                            case 9 -> System.out.println("-nine");
-                            default -> System.out.println("out of ability!");
+                            case 1:
+                                System.out.println("-one");
+                                break;
+                            case 2:
+                                System.out.println("-two");
+                                break;
+                            case 3:
+                                System.out.println("-three");
+                                break;
+                            case 4:
+                                System.out.println("-four");
+                                break;
+                            case 5:
+                                System.out.println("-five");
+                                break;
+                            case 6:
+                                System.out.println("-six");
+                                break;
+                            case 7:
+                                System.out.println("-seven");
+                                break;
+                            case 8:
+                                System.out.println("-eight");
+                                break;
+                            case 9:
+                                System.out.println("-nine");
+                                break;
+                            default:
+                                System.out.println("out of ability!");
                         }
                     }
                 }
