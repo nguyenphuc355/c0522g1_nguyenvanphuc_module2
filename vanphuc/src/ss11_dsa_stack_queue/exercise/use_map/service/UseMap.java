396423250df5691a1ba5e5d2a0d2b241720bd34e
nguyenphuc355 <nguyenvanphuca9999@gmail.com>
2022-07-18 16:27:01 +0700

    update

--- vanphuc/src/ss11_dsa_stack_queue/exercise/use_map/service/UseMap.java
+++ vanphuc/src/ss11_dsa_stack_queue/exercise/use_map/service/UseMap.java
@@ -12,16 +12,22 @@ public class UseMap {
         String string = scanner.nextLine();
         String[] array;
         array = string.trim().toLowerCase().split(" ");
+
         Map<String, Integer> treeMap = new TreeMap<>();
+
         for (String key : array) {
             if (treeMap.containsKey(key)) {
+
                 treeMap.put(key, treeMap.get(key) + 1);
+
             } else {
                 treeMap.put(key, 1);
             }
         }
-        System.out.println("So lan xuat hien cu moi tu la");
-        System.out.println(treeMap);
+        System.out.println("So lan xuat hien cua moi tu la");
+        for (Map.Entry<String, Integer> entry : treeMap.entrySet()){
+            System.out.printf("%s-%s\n",entry.getKey(),entry.getValue());
+        }
     }
 }
 
